f310d0f4e9a9567fe90796d576cb9bb87d1d46bb
lavuminhtriet <ngoaisauyeudau@gmail.com>
2026-05-02 20:18:39 +0700

    feat(auth): add JWT auth and RBAC

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { SupabaseModule } from './supabase/supabase.module';
       envFilePath: '.env',
     }),
     SupabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
